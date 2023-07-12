2acb21d65f721ae792895c92493bf35104509725
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-07-12 08:53:22 +0600

    complete eslint prettier husky lint-staged\

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 
 // cors
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
